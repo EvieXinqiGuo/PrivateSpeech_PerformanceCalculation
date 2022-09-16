c686ece8a1fc8b5254af734f8814ebeabb4c9dd8
xinqi <guoxinqieve@gmail.com>
2022-09-16 02:26:08 -0700

    updating a mistake

--- PerformanceCalculation.R
+++ PerformanceCalculation.R
@@ -1,19 +1,20 @@
 # load the dataset(s)
-setwd("~PrivateSpeech_PerformanceCalculation")
+getwd()
+setwd("/Users/guoxinqieve/Library/CloudStorage/OneDrive-UCSanDiego/Dissertation/descriptive_survey_data_nnpp/PrivateSpeech_PerformanceCalculation")
 library(tidyverse)
 library(readr)
 library(stringr)
 
 
 performance_calculation = function(x){
-  DF = read_csv(x, col_names = FALSE)
+  DF = read_csv(x, col_names = FALSE) # load CSV
 test_processing = DF %>% 
-  mutate(across(where(is.character), str_remove_all, pattern = fixed(" "))) %>%
-  mutate(tap_order = 1:n()) %>%
-  mutate(pair_order = (tap_order +1)%/%2) %>% # track back to which are pairs 
-  mutate(tap_sequence = accumulate(X1, c)) %>%
-  mutate(times_appeared = as.numeric(ave(X1, X1, FUN = seq_along)) - 1) %>%
-  group_by(pair_order) %>%
+  mutate(across(where(is.character), str_remove_all, pattern = fixed(" "))) %>% # remove the space for words with two words 
+  mutate(tap_order = 1:n()) %>% # create a tap order/sequence for later reference 1) the odd and the even are pairs
+  mutate(pair_order = (tap_order +1)%/%2) %>% # track back to which are pairs 1) assigning the order of turning.
+  mutate(tap_sequence = accumulate(X1, c)) %>% # 
+  mutate(times_appeared = as.numeric(ave(X1, X1, FUN = seq_along)) - 1) %>% # accumulating the times the an element has appeared before
+  group_by(pair_order) %>% # grouping by pair_order to figure out if it is a match or not
   mutate(match_or_not = case_when( # seeing if each pair is a match or not
     length(unique(X1)) == 1 ~ "match",
     TRUE ~ "mismatch")) %>%
@@ -32,15 +33,15 @@ participant_performance = as.data.frame(table(table_clean$categorized_pair)) %>%
 participant_performance$categorized_pair = factor(participant_performance$categorized_pair, 
                                                   levels = c("unlucky_no_info", "lucky_match", "other"))
 
-performance = nrow(table_clean) + sum(participant_performance$count_category[participant_performance$categorized_pair != "other"]) - 24
+performance = nrow(table_clean)/2 + sum(participant_performance$count_category[participant_performance$categorized_pair != "other"]) - 24
 return(performance)
 }
 
-L = list.files("~PrivateSpeech_PerformanceCalculation", ".csv")
+L = list.files("/Users/guoxinqieve/Library/CloudStorage/OneDrive-UCSanDiego/Dissertation/descriptive_survey_data_nnpp/PrivateSpeech_PerformanceCalculation", ".csv")
 
 performance_calcuated = lapply(L, performance_calculation)
 
 merged_performance_calcuated =  do.call(rbind, performance_calcuated)
 merged_df = cbind(as.data.frame(L) ,merged_performance_calcuated)
 
-write.csv(merged_df,"~/merged_performance_calcuated.csv", row.names = FALSE)
+write.csv(merged_df,"/Users/guoxinqieve/Library/CloudStorage/OneDrive-UCSanDiego/Dissertation/descriptive_survey_data_nnpp/PrivateSpeech_PerformanceCalculation/merged_performance_calcuated.csv", row.names = FALSE)
